20cbd1b81206331774c9299bd9e425e92ad9bfa5
feigme <feigme@gmail.com>
2023-07-17 20:46:23 +0800

    test运行成功

--- show-examples/src/main/java/com/wfc/show/web/config/WebConfig.java
+++ show-examples/src/main/java/com/wfc/show/web/config/WebConfig.java
@@ -37,45 +37,4 @@ public class WebConfig implements WebMvcConfigurer {
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
-    @Override
-    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
-
-        List<MediaType> mediaTypes = new ArrayList<>();
-        mediaTypes.add(MediaType.APPLICATION_JSON);
-        mediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
-        mediaTypes.add(MediaType.TEXT_HTML);
-        mediaTypes.add(new MediaType("application", "xml"));
-        mediaTypes.add(new MediaType("text", "xml"));
-        mediaTypes.add(new MediaType("application", "*+xml"));
-        mediaTypes.add(MediaType.ALL);
-        fastJsonHttpMessageConverter.setSupportedMediaTypes(mediaTypes);
-
-        FastJsonConfig fastJsonConfig = new FastJsonConfig();
-        fastJsonConfig.setCharset(Charset.forName("UTF-8"));
-        fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
-        SerializerFeature[] serializerFeatures = new SerializerFeature[]{
-                // 格式化
-                SerializerFeature.PrettyFormat,
-                // 输出空值
-                SerializerFeature.WriteMapNullValue,
-                // 空集合输出[]
-                SerializerFeature.WriteNullListAsEmpty,
-                // 日期格式化
-                SerializerFeature.WriteDateUseDateFormat
-        };
-        fastJsonConfig.setSerializerFeatures(serializerFeatures);
-
-
-        SerializeConfig serializeConfig = SerializeConfig.globalInstance;
-        serializeConfig.put(BigInteger.class, ToStringSerializer.instance);
-        serializeConfig.put(Long.class, ToStringSerializer.instance);
-        serializeConfig.put(Long.TYPE, ToStringSerializer.instance);
-        fastJsonConfig.setSerializeConfig(serializeConfig);
-
-        fastJsonHttpMessageConverter.setFastJsonConfig(fastJsonConfig);
-
-        converters.add(0, fastJsonHttpMessageConverter);
-    }
-
 }
